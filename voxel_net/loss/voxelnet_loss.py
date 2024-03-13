194783add491bb0f608b60a599b04af4858cab6a
neetmehta <neetmehta19@gmail.com>
2024-03-12 23:27:12 -0500

    implemented the loss function

--- voxel_net/loss/voxelnet_loss.py
+++ voxel_net/loss/voxelnet_loss.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 class VoxelnetLoss(nn.Module):
     
@@ -7,9 +8,23 @@ class VoxelnetLoss(nn.Module):
         super().__init__()
         self.alpha = alpha
         self.beta = beta
+        self.smoothl1loss = nn.SmoothL1Loss(size_average=False)
         
-    def forward(self, pred_score, pred_reg, pos, neg, reg):
+    def forward(self, pred_reg, pred_score, pos, neg, reg):
         
-        pass
+        pred_reg = pred_reg.permute(0,2,3,1)
+        pred_reg = pred_reg.reshape(*pred_reg.shape[:3],-1,7)
+        reg = reg.reshape(*reg.shape[:3],-1,7)
+        pred_score = pred_score.permute(0,2,3,1)
+        pred_score = F.sigmoid(pred_score)
+        where_pos = torch.where(pos==1)
+        reg_loss = self.smoothl1loss(pred_reg[where_pos], reg[where_pos])/pos.sum()
+
+        pos_loss = F.binary_cross_entropy(pred_score[pos==1], pos[pos==1])
+        neg_loss = F.binary_cross_entropy(1-pred_score[neg==1], neg[neg==1])
         
+        L_cls = self.alpha*pos_loss + self.beta*neg_loss
+        L_reg = reg_loss
+        
+        return L_cls, L_reg
         
